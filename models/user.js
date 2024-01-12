7ea3ed918b413ef3c439b6030f40990bf507e41c
jwatsoncodes <149508637+jwatsoncodes@users.noreply.github.com>
2024-01-12 10:06:50 -0500

    bcrypt added to appropriate files

--- models/user.js
+++ models/user.js
@@ -1,23 +1,13 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection"); // fix this
+const sequelize = require("../config/connection"); // Replace "../config/connection" with the correct path
+const bcrypt = require("bcrypt");
 
-<<<<<<< HEAD
 class User extends Model {
-    // Method to check the user's password
-    async checkPassword(password) {
-      return await bcrypt.compare(password, this.password);
-    }
+  // Method to check the user's password
+  async checkPassword(password) {
+    return await bcrypt.compare(password, this.password);
   }
-=======
-// class User extends Model {
-//   // Method to check the user's password
-//   async checkPassword(password) {
-//     return await bcrypt.compare(password, this.password);
-//   }
-// }
->>>>>>> 2727dfa4da554df00c4835edaca8f703fbbf4872
-
-class User extends Model {}
+}
 
 User.init(
   {
@@ -44,44 +34,21 @@ User.init(
         isEmail: true,
       },
     },
-<<<<<<< HEAD
-    {
-      hooks: {
-        async beforeCreate(user) {
-          user.password = await bcrypt.hash(user.password, 10);
-          return user;
-        },
-        async beforeUpdate(user) {
-          user.password = await bcrypt.hash(user.password, 10);
-          return user;
-        },
-      },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: "users",
-    }
-=======
   },
   {
-    //   hooks: {
-    //     async beforeCreate(user) {
-    //       user.password = await bcrypt.hash(user.password, 10);
-    //       return user;
-    //     },
-    //     async beforeUpdate(user) {
-    //       user.password = await bcrypt.hash(user.password, 10);
-    //       return user;
-    //     },
-    //   },
+    hooks: {
+      async beforeCreate(user) {
+        user.password = await bcrypt.hash(user.password, 10);
+        return user;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: "users",
   }
->>>>>>> 2727dfa4da554df00c4835edaca8f703fbbf4872
 );
 
 module.exports = User;
+
